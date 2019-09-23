a2bd8da02e35521b3dc41216f16cb1268c5332ef
devopscolombia <31006597+devopscolombia@users.noreply.github.com>
2019-09-23 14:39:35 -0500

    Update ec2.tf
    deleted filter

--- ec2.tf
+++ ec2.tf
@@ -9,12 +9,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   owners = ["895544890250"]
 
-    filter {
-    name   = "virtualization-type"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenaial-16.04-amd64-server-*"]
-  }
-
-  }
+    }
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
