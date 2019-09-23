79586cfda03ab4d710baa6f81791d798be5a6400
devopscolombia <31006597+devopscolombia@users.noreply.github.com>
2019-09-23 14:59:14 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version = "~> 2.29"
-  region = "eu-west-2"
+  region = "eu-west-1"
   access_key = "AKIA5BAVCSOFEDAZX6UR"
   secret_key = "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"
 }
@@ -8,10 +8,16 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners = ["895544890250"]
+data "aws_availability_zone" "az" {
+}
+}
 
-    }
+filter {
+  name = "name"
+  values = ["ubuntu/images/hvm-ssd/ubuntu-xenaial-16.04-amd64-server-*"]
+  }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web-server" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
